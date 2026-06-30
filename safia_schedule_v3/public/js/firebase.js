6ccbfc8d766338d0d9f94f51ce70a16162efaca8
yugen1222 <134773110+yugen1222@users.noreply.github.com>
2026-06-30 23:56:15 +0500

    Refactor Firebase initialization and configuration

--- safia_schedule_v3/public/js/firebase.js
+++ safia_schedule_v3/public/js/firebase.js
@@ -1,28 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import {
-    getDatabase,
-    ref,
-    get,
-    set,
-    onValue
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBRvy5BT6QxSuT5MUCfLeWi51P_9Fkxa3c",
-    authDomain: "safia-schedule.firebaseapp.com",
-    databaseURL: "https://safia-schedule-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "safia-schedule",
-    storageBucket: "safia-schedule.firebasestorage.app",
-    messagingSenderId: "125156028704",
-    appId: "1:125156028704:web:0b26fdf8e73f6fad362745"
+  apiKey: "AIzaSyBRvy5BT6QxSuT5MUCfLeWi51P_9Fkxa3c",
+  authDomain: "safia-schedule.firebaseapp.com",
+  databaseURL: "https://safia-schedule-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "safia-schedule",
+  storageBucket: "safia-schedule.firebasestorage.app",
+  messagingSenderId: "125156028704",
+  appId: "1:125156028704:web:0b26fdf8e73f6fad362745"
 };
 
-const app = initializeApp(firebaseConfig);
-
-const db = getDatabase(app);
-
-window.firebaseDB = db;
-window.firebaseRef = ref;
-window.firebaseSet = set;
-window.firebaseGet = get;
-window.firebaseOnValue = onValue;
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.database();
+window.SAFIA_PATH = "safia_schedule/sergeli";
+window.CLIENT_ID = localStorage.getItem("clientId") || Date.now().toString();
+localStorage.setItem("clientId", window.CLIENT_ID);
